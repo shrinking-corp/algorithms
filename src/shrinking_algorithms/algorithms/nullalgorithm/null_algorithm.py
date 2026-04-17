f6fe8be3acd9eacba0ba1333556af815d536bbe3
markooda <markooda@proton.me>
2026-04-17 18:37:41 +0200

    feat: implement null object for no algorithm as a means of supporting the preprocessing decorator

---
+++ src/shrinking_algorithms/algorithms/nullalgorithm/null_algorithm.py
@@ -0,0 +1,13 @@
+from typing import Any, Dict
+from shrinking_algorithms.algorithms.abstract_algorithm import Algorithm
+
+class NullAlgorithm(Algorithm):
+    """
+    Use no algorithm for diagram shrinking. Useful for serving as a wrappee for 
+    the Preprocessing decorator utilizing Null Object pattern. 
+    """
+    def initialize(self, **params: Any) -> None:
+        pass
+
+    def compute(self, parsed_puml: Dict[str, Any]) -> Dict[str, Any]:
+        return parsed_puml
